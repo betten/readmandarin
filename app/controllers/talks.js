0dcb0de3ff91497d67a44b0f21a7442589e1e44b
betten <michael.betten@gmail.com>
2014-12-23 15:31:36 -0500

    added talks#show route/action, added basic talk loading to talks controller

--- app/controllers/talks.js
+++ app/controllers/talks.js
@@ -1,3 +1,13 @@
+var Talk = require('../models/talk');
+
 exports.index = function(req, res) {
-  res.send('hello talks');
+  Talk.find({}, 'id ted_talk_id title', function(error, talks) {
+    res.send(talks);
+  });
+};
+
+exports.show = function(req, res) {
+  Talk.findOne({ ted_talk_id: req.params.id }, function(error, talk) {
+    res.send(talk);
+  });
 };
--- config/routes.js
+++ config/routes.js
@@ -2,4 +2,5 @@ var talks = require('../app/controllers/talks');
 
 module.exports = function(app) {
   app.get('/', talks.index);
+  app.get('/talks/:id', talks.show);
 };
