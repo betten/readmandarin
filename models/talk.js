f5a684f79263f48835c27fd74499c2dac51820e0
betten <michael.betten@gmail.com>
2014-12-23 09:59:37 -0500

    added count to talk schema and pre save hook to warn if counts not the same

--- models/talk.js
+++ models/talk.js
@@ -7,16 +7,25 @@ var TalkSchema = new Schema({
   subtitles: {
     en: {
       captions: Schema.Types.Mixed,
-      text: Schema.Types.Mixed
+      text: Schema.Types.Mixed,
+      count: Number
     },
     zh: {
       captions: Schema.Types.Mixed,
       text: Schema.Types.Mixed,
       html: Schema.Types.Mixed,
-      vocab: Schema.Types.Mixed
-    },
-    count: Number
+      vocab: Schema.Types.Mixed,
+      count: Number
+    }
   }
+});
+
+TalkSchema.pre('save', function(next) {
+  if(this.subtitles.zh.count != undefined && this.subtitles.en.count != undefined && this.subtitles.zh.count != this.subtitles.en.count) {
+    console.warn('Subtitles zh count != en count for talk ' + this.id);
+  }
+
+  next();
 });
 
 var Talk = mongoose.model('Talk', TalkSchema);
