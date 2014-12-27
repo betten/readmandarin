4fb35f50a465dc343afdfdeb015cda654bb6ee23
betten <michael.betten@gmail.com>
2014-12-27 13:43:41 -0500

    updated talks controller to check if talk exists before adding, updated talks view to display talk name

--- app/controllers/talks.js
+++ app/controllers/talks.js
@@ -1,8 +1,15 @@
 var Talk = require('../models/talk'),
     RM = require('../../lib/readmandarin');
 
+var exists = function(ted_talk_id, callback) {
+  Talk.count({ ted_talk_id: ted_talk_id }, function(error, count) {
+    if(error) return callback(error);
+    callback(error, !!count);
+  });
+};
+
 exports.index = function(req, res) {
-  Talk.find({}, 'id ted_talk_id title', function(error, talks) {
+  Talk.find({}, 'id ted_talk_id name', function(error, talks) {
     if(error) return res.send(error);
     res.json(talks);
   });
@@ -16,15 +23,28 @@ exports.show = function(req, res) {
 };
 
 exports.add = function(req, res) {
-  RM.getAndSave(req.param('ted_talk_id'), function(error, talk) {
+  var ted_talk_id = req.param('ted_talk_id');
+
+  exists(ted_talk_id, function(error, exists) {
     if(error) return res.send(error);
-    res.json({ id: talk.id, ted_talk_id: talk.ted_talk_id });
+
+    if(exists) {
+      res.json({ ted_talk_id: ted_talk_id });
+    }
+    else {
+      RM.getAndSave(ted_talk_id, function(error, talk) {
+        if(error) return res.send(error);
+        res.json({ ted_talk_id: talk.ted_talk_id });
+      });
+    }
   });
 };
 
 exports.exists = function(req, res) {
-  Talk.count({ ted_talk_id: req.params.ted_talk_id }, function(error, count) {
+  var ted_talk_id = req.params.ted_talk_id;
+
+  exists(ted_talk_id, function(error, exists) {
     if(error) return res.send(error);
-    res.send({ ted_talk_id: req.params.ted_talk_id, exists: !!count });
+    res.json({ ted_talk_id: ted_talk_id, exists: exists });
   });
 };
